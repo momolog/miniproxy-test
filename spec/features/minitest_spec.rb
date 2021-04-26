3127564d93ef0494e876e7e9da03c7c50f7aed93
Alexander Presber <alexander@giantmonkey.de>
2021-04-26 20:45:45 +0200

    replace miniproxy with evil-proxy

--- spec/features/minitest_spec.rb
+++ spec/features/minitest_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature 'proxy_test', js: true do
 
   it 'shows an external site' do
-    MiniProxy.allow_request(method: "GET", url: /heise.de/)
     visit 'https://www.heise.de'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,12 +8,37 @@ require 'capybara/rspec'
 require 'capybara/rails'
 
 require "selenium/webdriver"
-require "miniproxy"
+
+require 'evil-proxy'
+require 'evil-proxy/selenium'
+
+trap "INT"  do proxy.shutdown end
+trap "TERM" do proxy.shutdown end
+
+proxy = EvilProxy::HTTPProxyServer.new Port: 8080
+proxy.start
+
+proxy.before_request do |req|
+  # Do evil things
+  # Note that, different from Webrick::HTTPProxyServer, 
+  #   `req.body` is writable.
+  p "######### request"
+  p req
+  p "#########"
+end
+
+proxy.before_response do |req, res|
+  p "######### response"
+  p res
+  p "#########"
+  res.body = 'You have been hacked'
+end
 
 Capybara.register_driver :chrome do |app|
+
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     "goog:chromeOptions": {
-      args: ["ignore-certificate-errors", "disable-web-security", "proxy-server=#{MiniProxy.host}:#{MiniProxy.port}"]
+      args: ["ignore-certificate-errors", "disable-web-security", "proxy-server=127.0.0.1:8080"]
     }
   )
   Capybara::Selenium::Driver.new(app,
